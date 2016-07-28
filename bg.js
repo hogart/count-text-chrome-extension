25b7291ab76ea91c13657d2ec048e3c19f12bd55
Konstantin Kitmanov <doctor.hogart@gmail.com>
2016-07-28 18:07:47 +0200

    First commit ever

---
+++ bg.js
@@ -0,0 +1,61 @@
+(() => {
+    'use strict';
+
+    const notificationId = 'textStatsNotification';
+
+    function showStats(text) {
+        //Clear old notification
+        chrome.notifications.clear(notificationId, wasCleared => {});
+
+        const stats = calculateStats(text);
+        const itemsOptions = [
+            {
+                title: chrome.i18n.getMessage('charsWoSpaces'),
+                message: stats.charactersAmountWithoutSpaces.toString(),
+            },
+            {
+                title: chrome.i18n.getMessage('uniqueWordsTitle'),
+                message: chrome.i18n.getMessage('uniqueWordsMsg', [stats.uniqueWordAmount, stats.uniqueWordsPercentage]),
+            },
+            {
+                title: chrome.i18n.getMessage('avgWordLength'),
+                message: stats.avgWordLength.toString(),
+            },
+            {
+                title: chrome.i18n.getMessage('sentencesCount'),
+                message: stats.sentenceAmount.toString(),
+            },
+            {
+                title: chrome.i18n.getMessage('avgSentenceLength'),
+                message: stats.avgSentenceLength.toString(),
+            },
+        ];
+        const summary = chrome.i18n.getMessage('summary', [stats.wordAmount, stats.charactersAmount]);
+        const notificationOptions = {
+            title: summary,
+            message: summary,
+            type: 'list',
+            iconUrl: 'icon.png',
+            items: itemsOptions,
+            // buttons: [],
+            isClickable: true
+        };
+
+        chrome.notifications.create(notificationId, notificationOptions, notificationID => {});
+    }
+
+    function onContextMenuClick({selectionText}) {
+        showStats(selectionText);
+    }
+
+    function createMenu() {
+        chrome.contextMenus.create({
+            title: 'Text stats',
+            id: 'parent',
+            contexts: ['selection']
+        });
+    }
+
+    chrome.contextMenus.onClicked.addListener(onContextMenuClick);
+    chrome.runtime.onInstalled.addListener(createMenu);
+})();
---
+++ ew.js
@@ -0,0 +1,39 @@
+(() => {
+    'use strict';
+
+    const observer = new MutationObserver(() => {
+        const stats = calculateStats(observeTarget.textContent);
+        const summary = chrome.i18n.getMessage('summary', [stats.wordAmount, stats.charactersAmount]);
+
+        displayTarget.dataset.ewStats = summary;
+    });
+
+    const observeSettings = {
+        childList: true,
+        subtree: true,
+    };
+
+    let displayTarget;
+    let observeTarget;
+
+    function getEditor() {
+        const iframe = document.querySelector('iframe[name$="common-editor-iframe"]');
+        if (iframe && iframe.contentWindow.document) {
+            return iframe.contentWindow.document.querySelector('div[contenteditable]');
+        } else {
+            return null;
+        }
+    }
+
+    function tryObserve() {
+        observeTarget = getEditor();
+        if (observeTarget) {
+            displayTarget = document.querySelector('[tabindex="0"] > input + input').parentNode;
+            observer.observe(observeTarget, observeSettings);
+        } else {
+            setTimeout(tryObserve, 150);
+        }
+    }
+
+    tryObserve();
+})();
